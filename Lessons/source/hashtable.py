37495894bf2c5a8ac57224d4ecdc7c85cc85fdce
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-04-21 23:56:11 -0700

    🍽  hashtable implemented still need to do time and space complexity analysis

--- Lessons/source/hashtable.py
+++ Lessons/source/hashtable.py
@@ -26,8 +26,9 @@ class HashTable(object):
     def load_factor(self):
         """Return the load factor, the ratio of number of entries to buckets.
         Best and worst case running time: ??? under what conditions? [TODO]"""
-        # TODO: Calculate load factor
-        # return ...
+        if len(self.buckets) == 0:
+            raise AssertionError("HashTable is empty.")
+        return self.size / len(self.buckets)
 
     def keys(self):
         """Return a list of all keys in this hash table.
@@ -62,12 +63,17 @@ class HashTable(object):
         """Return the number of key-value entries by traversing its buckets.
         Best and worst case running time: ??? under what conditions? [TODO]"""
         # Count number of key-value entries in each of the buckets
-        item_count = 0
-        for bucket in self.buckets:
-            item_count += bucket.length()
-        return item_count
-        # Equivalent to this list comprehension:
-        return sum(bucket.length() for bucket in self.buckets)
+        if self.size:
+            return self.size
+        else:
+            item_count = 0
+            for bucket in self.buckets:
+                item_count += bucket.length()
+            return item_count
+            # Equivalent to this list comprehension:
+            return sum(bucket.length() for bucket in self.buckets)
+
+
 
     def contains(self, key):
         """Return True if this hash table contains the given key, or False.
@@ -111,12 +117,14 @@ class HashTable(object):
             # In this case, the given key's value is being updated
             # Remove the old key-value entry from the bucket first
             bucket.delete(entry)
+            self.size -= 1 # decrement size property
         # Insert the new key-value entry into the bucket in either case
         bucket.append((key, value))
-        # TODO: Check if the load factor exceeds a threshold such as 0.75
-        # ...
-        # TODO: If so, automatically resize to reduce the load factor
-        # ...
+        self.size += 1 # increment size property
+        # Check if the load factor exceeds a threshold such as 0.75
+        if self.load_factor() > 0.75:
+            # If so, automatically resize to reduce the load factor
+            self._resize()
 
     def delete(self, key):
         """Delete the given key and its associated value, or raise KeyError.
@@ -130,6 +138,7 @@ class HashTable(object):
         if entry is not None:  # Found
             # Remove the key-value entry from the bucket
             bucket.delete(entry)
+            self.size -= 1 # decrement size property
         else:  # Not found
             raise KeyError('Key not found: {}'.format(key))
 
@@ -145,13 +154,19 @@ class HashTable(object):
         # Option to reduce size if buckets are sparsely filled (low load factor)
         elif new_size is 0:
             new_size = len(self.buckets) / 2  # Half size
-        # TODO: Get a list to temporarily hold all current key-value entries
-        # ...
-        # TODO: Create a new list of new_size total empty linked list buckets
-        # ...
-        # TODO: Insert each key-value entry into the new list of buckets,
-        # which will rehash them into a new bucket index based on the new size
-        # ...
+
+        # Get a list to temporarily hold all current key-value entries
+        pairs_list = self.items() # get all key-value pairs from hashtable
+
+        # Create a new list of new_size total empty linked list buckets
+        self.buckets = [LinkedList() for i in range(new_size)] # same as init
+        self.size = 0 # reset size
+
+        # Insert each key-value entry into the new list of buckets, which will
+        # rehash them into a new bucket index based on the new size
+        for key, value in pairs_list:
+            # this will reset the overall size
+            self.set(key, value)
 
 
 def test_hash_table():
