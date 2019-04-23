0e6e272200f50a0d2be681089457434d8a8dac90
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-04-23 09:56:14 -0700

    👍🏾 added starter code for set class

---
+++ Lessons/source/set.py
@@ -0,0 +1,62 @@
+
+from hashtable import HashTable
+
+class Set(object):
+
+    def __init__(self, elements=None):
+        pass
+
+    def size(self):
+        """
+        property that tracks the number of elements in constant time
+        """
+        pass
+
+    def contains(self, element):
+        """
+        return a boolean indicating whether element is in this set
+        TODO: Analyze Time and Space Complexity
+        """
+        pass
+
+    def add(self, element):
+        """
+        add element to this set, if not present already
+        TODO: Analyze Time and Space Complexity
+        """
+        pass
+
+    def remove(self, element):
+        """
+        remove element from this set, if present, or else raise KeyError
+        TODO: Analyze Time and Space Complexity
+        """
+        pass
+
+    def union(self, other_set):
+        """
+        return a new set that is the union of this set and other_set
+        TODO: Analyze Time and Space Complexity
+        """
+        pass
+
+    def intersection(self, other_set):
+        """
+        return a new set that is the intersection of this set and other_set
+        TODO: Analyze Time and Space Complexity
+        """
+        pass
+
+    def difference(self, other_set):
+        """
+        return a new set that is the difference of this set and other_set
+        TODO: Analyze Time and Space Complexity
+        """
+        pass
+
+    def is_subset(self, other_set):
+        """
+        return a boolean indicating whether other_set is a subset of this set
+        TODO: Analyze Time and Space Complexity
+        """
+        pass
